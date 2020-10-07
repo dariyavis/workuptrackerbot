41f4c8180f4c16790730d4b445da00d8581223db
dariyavis <dariavis@mail.ru>
2020-10-07 03:03:19 +0300

    Initial commit. Spring Boot Starter

---
+++ src/test/java/com/workuptrackerbot/pack/WorkuptrackerbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workuptrackerbot.pack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkuptrackerbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
