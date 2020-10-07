a5f3b2408b3514195b3cd28c87bfd9506ba45e3c
dariyavis <dariavis@mail.ru>
2020-10-07 04:42:13 +0300

    Start bot api. Same answer

--- src/main/java/com/workuptrackerbot/ConfigXML.java
+++ src/main/java/com/workuptrackerbot/ConfigXML.java
@@ -1,18 +1,15 @@
 package com.workuptrackerbot;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.TelegramBotsApi;
 
 @Configuration
 @ImportResource("classpath:beans.xml")
 public class ConfigXML {
 
-//    @Bean
-//    public TelegramBotsApi telegramBotsApi() {
-//       // ApiContextInitializer.init();
-//        return new TelegramBotsApi();
-//    }
+    //    @Bean
+    //    public TelegramBotsApi telegramBotsApi() {
+    //       // ApiContextInitializer.init();
+    //        return new TelegramBotsApi();
+    //    }
 }
